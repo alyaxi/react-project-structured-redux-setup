1aaaab1863ee120e5a7df7e2085f4b16ef9572bb
alyaxi <ali.zaman@quanrio.com>
2023-09-04 17:55:14 +0500

    Code of project setup structured with redux

---
+++ src/routes/AllRoutes.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Main from "../pages/Main";
+
+const AllRoutes = () => {
+  const { isLoggedIn } = useSelector(state => state.auth);
+  return (
+    <Routes>
+        <Route index path={`/login`} element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route
+          path={`/`}
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+    </Routes>
+  )
+}
+
+export default AllRoutes;
